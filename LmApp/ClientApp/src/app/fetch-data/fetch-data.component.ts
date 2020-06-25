8f7cf4a379c042286af3663a62849888d192e053
noemik <tucita14@yahoo.com>
2020-06-25 20:04:24 +0300

    models, migrations created

--- LmApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ LmApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -12,7 +12,7 @@ export class FetchDataComponent {
     http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
       this.forecasts = result;
     }, error => console.error(error));
-  }
+   }
 }
 
 interface WeatherForecast {
--- LmApp/Startup.cs
+++ LmApp/Startup.cs
@@ -63,7 +63,7 @@ namespace LmApp
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "wwwroot";
 
                 if (env.IsDevelopment())
                 {
